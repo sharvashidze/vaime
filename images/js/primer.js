9093a7991290b9fdc4ef81633d1809a202c55b16
beka sharvashidize <beka.sharvashidze@mail.ru>
2021-12-30 02:01:42 +0300

    1
    1

---
+++ images/js/primer.js
@@ -0,0 +1,24 @@
+const age = 15;
+const totalPrice = 200.74;
+const userName = 'beka';
+const massage = 'hi';
+const isOpen = true;
+const shouldConfirm = false;
+
+// const shouldRenew = confirm('Кети Ты Беку любишь?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('надо выбрать от 1 до 5');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = '20px';
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
+
+
+
+
+let elementHeight = '200.74px';
+
